2531abd763878605ea8501e08ecf0c8261bf8421
ydfq26 <157113263+ydfq26@users.noreply.github.com>
2024-01-22 15:38:42 +0800

    Update quan1.sh

--- quan1.sh
+++ quan1.sh
@@ -1,6 +1,6 @@
 echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 
 
